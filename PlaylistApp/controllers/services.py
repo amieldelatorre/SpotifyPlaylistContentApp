b47483a0ec3fd5939b5f94f15e2e7b0ccc1d769f
amieldelatorre <amieljames.delatorre@gmail.com>
2023-02-11 09:54:47 +1300

    Get all playlists for current user

--- PlaylistApp/controllers/services.py
+++ PlaylistApp/controllers/services.py
@@ -1,9 +1,23 @@
 import spotipy
-from flask import session
 from .authController import create_spotify_oauth
-from spotipy.oauth2 import SpotifyOAuth
 
 
-def get_playlists():
+def get_playlists(limit=50, offset=0):
     sp = spotipy.Spotify(oauth_manager=create_spotify_oauth())
-    results = sp.current_user_playlists(limit=50, offset=100)
+
+    results = {}
+    while True:
+        query = sp.current_user_playlists(limit=limit, offset=offset)
+        for item in query['items']:
+            results[item['id']] = {
+                'name': item['name'],
+                'total': item['tracks']['total'],
+                'external_url': item['external_urls']['spotify']
+            }
+
+        if query['next'] is not None:
+            offset += limit
+        else:
+            break
+    return results
+
