4a8e3180f07e7ae3b12e0fa706eaa46fbc5a0e0a
amieldelatorre <amieljames.delatorre@gmail.com>
2023-02-11 10:01:31 +1300

    downloading all the playlists of a user and the songs inside it

--- PlaylistApp/controllers/frontendController.py
+++ PlaylistApp/controllers/frontendController.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory, after_this_request, redirect, send_file
 from .authController import create_login_url, login_required
-from .services import get_playlists
+from .services import get_playlists, get_playlist_content, get_user_id
+import pathlib, os, tarfile, shutil
 
 frontend_blueprint = Blueprint('frontend_bp', __name__)
 
@@ -8,9 +9,9 @@ frontend_blueprint = Blueprint('frontend_bp', __name__)
 @frontend_blueprint.route("/")
 @login_required
 def hello():
-    get_playlists()
     return render_template('default.html',
-                           title='Home')
+                           title='Home',
+                           playlists=get_playlists())
 
 
 @frontend_blueprint.route("/login")
@@ -18,3 +19,74 @@ def login():
     return render_template('login.html',
                            title="Login",
                            login_link=create_login_url())
+
+
+@frontend_blueprint.route("/download")
+@login_required
+def download():
+    temp_folder_path = os.path.join(pathlib.Path().resolve(), "temp")
+    user_id = get_user_id()
+    temp_user_folder_path = os.path.join(temp_folder_path, user_id)
+
+    if os.path.exists(temp_user_folder_path):
+        shutil.rmtree(temp_user_folder_path)
+    os.mkdir(temp_user_folder_path)
+
+    playlists = get_playlists()
+    for key in playlists.keys():
+        playlists[key]["content"] = get_playlist_content(key)
+
+        restricted_chars = ['/', '\\', '<', '>', '"', ':', '|', '?', '*']
+        restricted_chars_in_playlist_name = [ char for char in restricted_chars if char in playlists[key]['name'] ]
+        if restricted_chars_in_playlist_name:
+            playlist_file_path = os.path.join(temp_user_folder_path, f"{key}.csv")
+        else:
+            playlist_file_path = os.path.join(temp_user_folder_path, playlists[key]['name'] + ".csv")
+            if os.path.exists(playlist_file_path):
+                playlist_file_path = os.path.join(temp_user_folder_path, f"{key}.csv")
+
+        write_playlist_to_file(playlist_file_path, playlists[key], key)
+
+    archived_file_name = f"{temp_user_folder_path}.tar.gz"
+
+    with tarfile.open(archived_file_name, "w:gz") as tar:
+        tar.add(temp_user_folder_path, arcname=os.path.basename(temp_folder_path))
+
+    shutil.rmtree(temp_user_folder_path)
+    os.remove(archived_file_name)
+
+    return send_file(archived_file_name)
+
+
+@frontend_blueprint.route("/delete")
+def delete():
+    temp_folder_path = os.path.join(pathlib.Path().resolve(), "temp")
+    user_id = get_user_id()
+    temp_user_folder_path = os.path.join(temp_folder_path, user_id)
+    archived_file_name = f"{temp_user_folder_path}.tar.gz"
+    os.remove(archived_file_name)
+    return redirect("/")
+
+
+def write_playlist_to_file(playlist_file_path, playlist, playlist_id):
+    with open(playlist_file_path, "w", encoding="utf-32") as file:
+        file.write(f"Playlist Name,{playlist['name']}\n")
+        file.write(f"Playlist Id,{playlist_id}\n")
+        file.write(f"Total number of tracks,{playlist['total']}\n")
+        for content in playlist['content']:
+            artists = ""
+
+            if content is None or content['track'] is None:
+                continue
+            elif content['track']['artists'] is None:
+                artists = "None"
+            else:
+                for i in range(len(content['track']['artists'])):
+                    artists += content['track']['artists'][i]['name']
+                    if i != len(content['track']['artists']) - 1:
+                        artists += "|"
+
+            if 'spotify' not in content['track']['external_urls']:
+                file.write(f"{content['track']['name']},{artists},None\n")
+            else:
+                file.write(f"{content['track']['name']},{artists},{content['track']['external_urls']['spotify']}\n")
